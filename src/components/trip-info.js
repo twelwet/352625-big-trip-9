568a8ddd8f959cc9576e476550a8405c9604a21c
Maksim Akimov <maksimakimov@mBookPro15.local>
2019-08-30 16:46:36 +0300

    Fixes Date format in header

--- src/components/trip-info.js
+++ src/components/trip-info.js
@@ -1,9 +1,11 @@
 // trip-info.js
 
+import moment from 'moment';
+
 const getTripInfoTemplate = (data) => `
   <div class="trip-info__main">
     <h1 class="trip-info__title">${data.cities[0]} — ... — ${data.cities[data.cities.length - 1]}</h1>
-    <p class="trip-info__dates">${data.dates[0].start} —&nbsp;${data.dates[data.dates.length - 1].end}</p>
+    <p class="trip-info__dates">${moment(data.dates[0].start).format(`MMM M`)} —&nbsp;${moment(data.dates[data.dates.length - 1].end).format(`M`)}</p>
   </div>
   <p class="trip-info__cost">
     Total: &euro; <span class="trip-info__cost-value">${data.totalPrice}</span>
