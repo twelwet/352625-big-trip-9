1e4fa4ed31fd10fcb9405240b699648e29d99e37
Maksim Akimov <maksimakimov@mBookPro15.local>
2019-09-01 23:35:25 +0300

    Simplifies trip-info component

--- src/components/trip-info.js
+++ src/components/trip-info.js
@@ -2,13 +2,13 @@
 
 import moment from 'moment';
 
-const getTripInfoTemplate = (info) => `
+const getTripInfoTemplate = ({cities, dates, totalPrice}) => `
   <div class="trip-info__main">
-    <h1 class="trip-info__title">${info.cities[0]} — ... — ${info.cities[info.cities.length - 1]}</h1>
-    <p class="trip-info__dates">${moment(info.dates[0].start).format(`MMM D`)} — ${moment(info.dates[info.dates.length - 1].end).format(`MMM D`)}</p>
+    <h1 class="trip-info__title">${cities[0]} — ... — ${cities[cities.length - 1]}</h1>
+    <p class="trip-info__dates">${moment(dates[0].start).format(`MMM D`)} — ${moment(dates[dates.length - 1].end).format(`MMM D`)}</p>
   </div>
   <p class="trip-info__cost">
-    Total: &euro; <span class="trip-info__cost-value">${info.totalPrice}</span>
+    Total: &euro; <span class="trip-info__cost-value">${totalPrice}</span>
   </p>
 `;
 
