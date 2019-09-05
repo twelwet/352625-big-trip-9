abb5719c5bf308f66fe5792d9fd398b27735581e
Maksim Akimov <maksimakimov@mBookPro15.local>
2019-09-05 13:55:15 +0200

    Adds TripControlsController

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -1,36 +1,30 @@
 // trip.js
 
-import {Position, render} from "../utils";
-import {filtersData, menuData} from "../components/mock-data";
-import TripSort from "../components/trip-sort";
-import TripDays from "../components/trip-days";
-import Menu from "../components/menu";
-import Filters from "../components/filters";
 import EventsController from "./events";
 import TripInfoController from "./trip-info";
+import TripControlsController from "./trip-controls";
 
 class TripController {
-  constructor(points, pointsInfo) {
+  constructor(menuData, filtersData, points, pointsInfo) {
+    this._menuData = menuData;
+    this._filtersData = filtersData;
     this._points = points;
     this._pointsInfo = pointsInfo;
   }
 
   init() {
     const mainElement = document.querySelector(`.trip-main`);
+
     const infoElement = mainElement.querySelector(`.trip-info`);
     const controlsElement = mainElement.querySelector(`.trip-controls`);
     const eventsElement = document.querySelector(`.trip-events`);
 
-    const menu = new Menu(menuData);
-    const filters = new Filters(filtersData);
-
-    render(controlsElement, menu.getElement(), Position.AFTERBEGIN);
-    render(controlsElement, filters.getElement(), Position.BEFOREEND);
-
     const tripInfoController = new TripInfoController(infoElement, this._points, this._pointsInfo);
-    tripInfoController.init();
-
+    const tripControlsController = new TripControlsController(controlsElement, this._menuData, this._filtersData);
     const eventsController = new EventsController(eventsElement, this._points, this._pointsInfo);
+
+    tripInfoController.init();
+    tripControlsController.init();
     eventsController.init();
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // main.js
 
-import {points, pointsInfo} from './components/mock-data.js';
+import {menuData, filtersData, points, pointsInfo} from './components/mock-data.js';
 import TripController from './controllers/trip.js';
 
-const tripController = new TripController(points, pointsInfo);
+const tripController = new TripController(menuData, filtersData, points, pointsInfo);
 
 tripController.init();
