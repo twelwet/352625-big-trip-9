d41e542a20158311ec34b27a4f58b685b93e65b2
Maksim Akimov <maksimakimov@mBookPro15.local>
2019-09-24 17:46:08 +0300

    Divides event code from eventsController

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -1,31 +1,13 @@
 // events.js
 
-import {getOptions, citiesList} from "../components/mock-data";
-import moment from 'moment';
-import TripEvent from "../components/trip-event";
-import TripEventEdit from "../components/trip-event-edit";
 import {Position, render, unrender} from "../utils";
 import NoEvents from "../components/no-events";
 import TripSort from "../components/trip-sort";
 import TripDays from "../components/trip-days";
 import TripList from "../components/trip-list.js";
+import EventController from './event.js';
 
 const Sort = {EVENT: `event`, TIME: `time`, PRICE: `price`};
-const FORM_OPTION_MASK = `event-offer-`;
-
-const getOptionsByTypeChange = (type, dataFromForm) => {
-  const optionsFromForm = [...dataFromForm]
-    .filter(([entry]) => entry.includes(FORM_OPTION_MASK))
-    .map((item) => item[0].slice(FORM_OPTION_MASK.length));
-
-  const options = JSON.parse(JSON.stringify(getOptions(type)));
-
-  for (const item of options) {
-    item.isChecked = optionsFromForm.includes(item.option);
-  }
-
-  return options;
-};
 
 class EventsController {
   constructor(container, points, pointsInfo) {
@@ -35,6 +17,10 @@ class EventsController {
     this._tripSort = new TripSort();
     this._tripDays = new TripDays(this._points, this._pointsInfo);
     this._tripList = new TripList();
+
+    this._subscriptions = [];
+    this._onChangeView = this._onChangeView.bind(this);
+    this._onDataChange = this._onDataChange.bind(this);
   }
 
   init() {
@@ -51,82 +37,22 @@ class EventsController {
     }
   }
 
-
-  _renderTripEvent(container, point, info) {
-
-    const tripEvent = new TripEvent(point, info);
-    const tripEventEdit = new TripEventEdit(point, info);
-
-    const onEscPress = (evt) => {
-      document.removeEventListener(`keydown`, onEscPress);
-      if (evt.keyCode === 27) {
-        container.replaceChild(tripEvent.getElement(), tripEventEdit.getElement());
-      }
-    };
-
-    const rollUpBtnElement = tripEvent.getElement().querySelector(`.event__rollup-btn`);
-
-    const editFormElement = tripEventEdit.getElement().querySelector(`.event--edit`);
-
-    const typesElements = [...editFormElement.querySelectorAll(`.event__type-input`)];
-
-    const cityField = editFormElement.querySelector(`.event__input--destination`);
-
-    rollUpBtnElement.addEventListener(`click`, () => {
-      const otherEvents = [...document.querySelectorAll(`.trip-events__item`)]
-        .filter((it) => it.querySelector(`.event__rollup-btn`) !== rollUpBtnElement);
-      console.log(otherEvents);
-
-      typesElements.forEach((item) => item.addEventListener(`click`, tripEventEdit.onTypeChange.bind(tripEventEdit)));
-      cityField.addEventListener(`blur`, tripEventEdit.onCityChange.bind(tripEventEdit));
-
-      container.replaceChild(tripEventEdit.getElement(), tripEvent.getElement());
-      document.addEventListener(`keydown`, onEscPress);
-    });
-
-    editFormElement.addEventListener(`submit`, (evt) => {
-      evt.preventDefault();
-      document.removeEventListener(`keydown`, onEscPress);
-
-      typesElements.forEach((item) => item.removeEventListener(`click`, tripEventEdit.onTypeChange.bind(tripEventEdit)));
-      cityField.removeEventListener(`blur`, tripEventEdit.onCityChange.bind(tripEventEdit));
-
-      const formData = new FormData(editFormElement);
-
-      const type = formData.get(`event-type`);
-
-      const saveCity = (city) => {
-        if (!citiesList[city]) {
-          return point.city;
-        }
-        return city;
-      };
-
-      const entry = {
-        id: point.id,
-        type,
-        city: saveCity(formData.get(`event-destination`)),
-        date: {
-          start: moment(formData.get(`event-start-time`)).valueOf(),
-          end: moment(formData.get(`event-end-time`)).valueOf()
-        },
-        price: Number(formData.get(`event-price`)),
-        options: getOptionsByTypeChange(type, formData)
-      };
-
-      this._points[this._points.findIndex((it) => it.id === point.id)] = entry;
-
-      this._unrenderBoard();
-
-      this._sortByType(this._getSortType());
-    });
-
-    render(container, tripEvent.getElement(), Position.BEFOREEND);
-  }
-
   _renderNoEvents(container) {
     const noEvents = new NoEvents();
     render(container, noEvents.getElement(), Position.BEFOREEND);
+  }
+
+  _onChangeView() {
+    this._subscriptions.forEach((it) => it());
+  }
+
+  _onDataChange(newData, oldData) {
+    this._points[this._points.findIndex((it) => it.id === oldData.id)] = newData;
+
+    this._unrenderBoard();
+
+    this._sortByType(this._getSortType());
+
   }
 
   _sortByType(type) {
@@ -153,7 +79,10 @@ class EventsController {
     const pointsByDays = Object.values(this._pointsInfo.getDaysToPoints(this._points));
 
     [...dayLists].forEach((dayList, index) => {
-      pointsByDays[index].forEach((dayPoint) => this._renderTripEvent(dayList, dayPoint, this._pointsInfo));
+      pointsByDays[index].forEach((dayPoint) => {
+        const eventController = new EventController(dayList, dayPoint, this._onDataChange, this._onChangeView);
+        this._subscriptions.push(eventController.setDefaultView.bind(eventController));
+      });
     });
   }
 
@@ -169,8 +98,8 @@ class EventsController {
     const sortedPoints = sortedPeriods.map((entry) => this._points.find((point) => point.id === entry[0]));
 
     sortedPoints
-      .forEach((point) => this._renderTripEvent(this._tripList.getElement()
-        .querySelector(`.trip-events__list`), point, this._pointsInfo));
+      .forEach((point) => new EventController(this._tripList.getElement()
+        .querySelector(`.trip-events__list`), point, this._onDataChange, this._onChangeView));
   }
 
   _sortByPrice() {
@@ -183,8 +112,8 @@ class EventsController {
     const sortedPoints = sortedPrices.map((price) => this._points.find((point) => point.price === price));
 
     sortedPoints
-      .forEach((point) => this._renderTripEvent(this._tripList.getElement()
-        .querySelector(`.trip-events__list`), point, this._pointsInfo));
+      .forEach((point) => new EventController(this._tripList.getElement()
+        .querySelector(`.trip-events__list`), point, this._onDataChange, this._onChangeView));
   }
 
   _getSortType() {
